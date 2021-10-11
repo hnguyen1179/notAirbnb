066a4960b85d8f0aa5a593df362644b4bfb42107
Duke <hnguyen1179@berkeley.edu>
2021-10-11 16:18:31 -0700

    Added outlien to clear dates button for ListingCalendar

--- web/src/components/ListingReviews/ListingReviewsDesktop.tsx
+++ web/src/components/ListingReviews/ListingReviewsDesktop.tsx
@@ -2,8 +2,17 @@ import { ReactComponent as StarSvg } from "../../assets/icons/filled-star.svg";
 import { ReviewsByListingIdQuery } from "../../generated/graphql";
 import ListingReviewsItem from "./ListingReviewsItem";
 
+interface IAverageScores {
+	cleanliness: number;
+	accuracy: number;
+	communication: number;
+	location: number;
+	checkin: number;
+	value: number;
+}
 interface Props {
 	averageScore: number;
+	averageScores: IAverageScores;
 	reviewsCount: number;
 	reviews: ReviewsByListingIdQuery;
 }
