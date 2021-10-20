b1d1e06e905158b53d215b57aea69af03e11f4e1
Duke <hnguyen1179@berkeley.edu>
2021-10-20 03:22:42 -0700

    Converted BrowserRouter to HashRouter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { AppStateProvider } from "./context/AppContext";
 
 import {
