c81001723f7ef8424b507a00a47303f3cc50c0a4
Duke <hnguyen1179@berkeley.edu>
2023-05-01 20:08:50 -0700

    Fix demo account login

--- web/src/components/Entry/EntryForm.tsx
+++ web/src/components/Entry/EntryForm.tsx
@@ -10,92 +10,93 @@ const VERIFIED = "verified";
 const UNVERIFIED = "unverified";
 
 interface FormValues {
-	email: string;
-	password: string;
-	passwordLogin: string;
-	firstName: string;
-	lastName: string;
-	birthday: string;
+  email: string;
+  password: string;
+  passwordLogin: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
 }
 
 interface Props {
-	initialEntry?: string;
-	isModal?: boolean;
+  initialEntry?: string;
+  isModal?: boolean;
 }
 
 function EntryForm({ initialEntry, isModal = true }: Props) {
-	const { entry, setOpen, demoClicked } = useModal();
-	const [showPassword, setShowPassword] = useState(false);
-	const [showHints, setShowHints] = useState(false);
-	const [verified, setVerified] = useState(initialEntry || entry);
+  const { entry, setOpen, demoClicked } = useModal();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showHints, setShowHints] = useState(false);
+  const [verified, setVerified] = useState(initialEntry || entry);
 
-	const form = useForm<FormValues>({
-		criteriaMode: "all",
-	});
+  const form = useForm<FormValues>({
+    criteriaMode: "all",
+  });
 
-	const clickShowPassword = () => {
-		setShowPassword((prev: boolean) => !prev);
-	};
+  const clickShowPassword = () => {
+    setShowPassword((prev: boolean) => !prev);
+  };
 
-	const resetForm = (option = { goBack: false }) => {
-		const { goBack } = option;
-		form.reset();
-		setVerified(UNVERIFIED);
-		if (!goBack) setOpen(false);
-	};
+  const resetForm = (option = { goBack: false }) => {
+    const { goBack } = option;
+    form.reset();
+    setVerified(UNVERIFIED);
+    if (!goBack) setOpen(false);
+  };
 
-	const handleCloseModal = () => {
-		setOpen(false);
-	};
+  const handleCloseModal = () => {
+    console.log("in fx handleCloseModal");
+    setOpen(false);
+  };
 
-	// Prevent the user from scrolling when demoClicked is true
-	useEffect(() => {
-		if (demoClicked) {
-			document.body.style.overflow = "hidden";
-		}
+  // Prevent the user from scrolling when demoClicked is true
+  useEffect(() => {
+    if (demoClicked) {
+      document.body.style.overflow = "hidden";
+    }
 
-		return () => {
-			document.body.style.overflow = "hidden";
-		};
-	}, [demoClicked]);
+    return () => {
+      document.body.style.overflow = "hidden";
+    };
+  }, [demoClicked]);
 
-	// Email validator stage
-	if (verified === UNVERIFIED)
-		return (
-			<Welcome
-				form={form}
-				setVerified={setVerified}
-				handleCloseModal={handleCloseModal}
-				isModal={isModal}
-			/>
-		);
+  // Email validator stage
+  if (verified === UNVERIFIED)
+    return (
+      <Welcome
+        form={form}
+        setVerified={setVerified}
+        handleCloseModal={handleCloseModal}
+        isModal={isModal}
+      />
+    );
 
-	// Email is validated, Password input stage before log in validations
-	if (verified === VERIFIED)
-		return (
-			<Login
-				form={form}
-				setVerified={setVerified}
-				showPassword={showPassword}
-				clickShowPassword={clickShowPassword}
-				resetForm={resetForm}
-				isModal={isModal}
-			/>
-		);
+  // Email is validated, Password input stage before log in validations
+  if (verified === VERIFIED)
+    return (
+      <Login
+        form={form}
+        setVerified={setVerified}
+        showPassword={showPassword}
+        clickShowPassword={clickShowPassword}
+        resetForm={resetForm}
+        isModal={isModal}
+      />
+    );
 
-	// Defaults to the sign up form
-	return (
-		<SignUp
-			form={form}
-			setVerified={setVerified}
-			showHints={showHints}
-			setShowHints={setShowHints}
-			showPassword={showPassword}
-			clickShowPassword={clickShowPassword}
-			resetForm={resetForm}
-			isModal={isModal}
-		/>
-	);
+  // Defaults to the sign up form
+  return (
+    <SignUp
+      form={form}
+      setVerified={setVerified}
+      showHints={showHints}
+      setShowHints={setShowHints}
+      showPassword={showPassword}
+      clickShowPassword={clickShowPassword}
+      resetForm={resetForm}
+      isModal={isModal}
+    />
+  );
 }
 
 export default EntryForm;
--- web/src/components/Entry/EntryFormWelcome.tsx
+++ web/src/components/Entry/EntryFormWelcome.tsx
@@ -14,125 +14,121 @@ const VERIFIED = "verified";
 const SIGNUP = "signup";
 
 function Welcome({ form, setVerified, handleCloseModal, isModal }: any) {
-	const { demoClicked, setDemoClicked } = useModal();
+  const { demoClicked, setDemoClicked } = useModal();
 
-	const {
-		handleSubmit,
-		setValue,
-		register,
-		setFocus,
-		getValues,
-		formState: { errors },
-	} = form;
+  const {
+    handleSubmit,
+    setValue,
+    register,
+    setFocus,
+    getValues,
+    formState: { errors },
+  } = form;
 
-	const [verifyEmail, { loading: verifyLoad }] = useVerifyEmail();
+  const [verifyEmail, { loading: verifyLoad }] = useVerifyEmail();
 
-	const onSubmitEmail = async (payload: any) => {
-		const { email } = payload;
-		const res = await verifyEmail(email);
-		setValue("passwordLogin", "");
-		setVerified(res.data.verifyEmail ? VERIFIED : SIGNUP);
-	};
+  const onSubmitEmail = async (payload: any) => {
+    const { email } = payload;
+    const res = await verifyEmail(email);
+    setValue("passwordLogin", "");
+    setVerified(res.data.verifyEmail ? VERIFIED : SIGNUP);
+  };
 
-	const clickDemo = async () => {
-		try {
-			setFocus("email");
-			await typeWriter("email", "demo.account@gmail.com", setValue);
-			await onSubmitEmail({ email: getValues("email") });
+  const clickDemo = async () => {
+    try {
+      setFocus("email");
+      await typeWriter("email", "ronald.cox@example.com", setValue);
+      await onSubmitEmail({ email: getValues("email") });
 
-			await sleep(500);
+      await sleep(500);
 
-			const submitPassword = document.querySelector(
-				".EntryForm__main__form__submit-button"
-			) as HTMLElement;
+      const submitPassword = document.querySelector(
+        ".EntryForm__main__form__submit-button"
+      ) as HTMLElement;
 
-			setFocus("passwordLogin");
-			await typeWriter("passwordLogin", "public", setValue);
-			submitPassword.click();
-		} catch (e) {
-			setDemoClicked(false);
-		}
-	};
+      setFocus("passwordLogin");
+      await typeWriter("passwordLogin", "public", setValue);
+      submitPassword.click();
+    } catch (e) {
+      setDemoClicked(false);
+    }
+  };
 
-	const inputProps = demoClicked ? { shrink: true } : undefined;
-	const hideClose = isModal ? "" : "hide";
+  const inputProps = demoClicked ? { shrink: true } : undefined;
+  const hideClose = isModal ? "" : "hide";
 
-	return (
-		<div className="EntryForm EntryForm--welcome">
-			<header>
-				<button
-					aria-label="Exit Button"
-					className={`EntryForm__close ${hideClose}`}
-					onClick={handleCloseModal}
-				>
-					<ExitSvg />
-				</button>
-				<h1 className="EntryForm__title">Log in or sign up</h1>
-				<div></div>
-			</header>
-			<main className="EntryForm__main">
-				<div className="EntryForm__main__subtitle">
-					<h3>Welcome to notAirbnb</h3>
-				</div>
-				<form
-					className="EntryForm__main__form"
-					onSubmit={handleSubmit(onSubmitEmail)}
-					autoComplete="off"
-				>
-					<div className="MuiContainer">
-						<TextField
-							error={!!errors.email}
-							helperText={
-								<FormError error={errors.email?.message} />
-							}
-							label="Email"
-							placeholder="Email"
-							variant="outlined"
-							fullWidth
-							InputLabelProps={inputProps}
-							{...register("email", {
-								required: "This field is required",
-								pattern: {
-									value: emailRegex,
-									message: "Must be a valid email address",
-								},
-							})}
-						/>
-					</div>
-					<div>
-						<SubmitButton loading={verifyLoad}>
-							Continue
-						</SubmitButton>
-					</div>
-				</form>
-				<div className="EntryForm__main__divider">
-					<span className="EntryForm__main__divider__or">or</span>
-				</div>
-				<div className="EntryForm__main__auth-container">
-					<button className="EntryForm__main__auth-container__auth EntryForm__main__auth-container__auth--google">
-						<div>
-							<GoogleSvg />
-						</div>
-						<div>Continue with Google</div>
-						<div></div>
-					</button>
-					<button
-						className="EntryForm__main__auth-container__auth EntryForm__main__auth-container__auth--demo"
-						onClick={() => {
-							setDemoClicked(true);
-							clickDemo();
-						}}
-					>
-						<div>
-							<DemoSvg style={{ width: "25px" }} />
-						</div>
-						<div>Use Demo Account</div>
-						<div></div>
-					</button>
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="EntryForm EntryForm--welcome">
+      <header>
+        <button
+          aria-label="Exit Button"
+          className={`EntryForm__close ${hideClose}`}
+          onClick={handleCloseModal}
+        >
+          <ExitSvg />
+        </button>
+        <h1 className="EntryForm__title">Log in or sign up</h1>
+        <div></div>
+      </header>
+      <main className="EntryForm__main">
+        <div className="EntryForm__main__subtitle">
+          <h3>Welcome to notAirbnb</h3>
+        </div>
+        <form
+          className="EntryForm__main__form"
+          onSubmit={handleSubmit(onSubmitEmail)}
+          autoComplete="off"
+        >
+          <div className="MuiContainer">
+            <TextField
+              error={!!errors.email}
+              helperText={<FormError error={errors.email?.message} />}
+              label="Email"
+              placeholder="Email"
+              variant="outlined"
+              fullWidth
+              InputLabelProps={inputProps}
+              {...register("email", {
+                required: "This field is required",
+                pattern: {
+                  value: emailRegex,
+                  message: "Must be a valid email address",
+                },
+              })}
+            />
+          </div>
+          <div>
+            <SubmitButton loading={verifyLoad}>Continue</SubmitButton>
+          </div>
+        </form>
+        <div className="EntryForm__main__divider">
+          <span className="EntryForm__main__divider__or">or</span>
+        </div>
+        <div className="EntryForm__main__auth-container">
+          <button className="EntryForm__main__auth-container__auth EntryForm__main__auth-container__auth--google">
+            <div>
+              <GoogleSvg />
+            </div>
+            <div>Continue with Google</div>
+            <div></div>
+          </button>
+          <button
+            className="EntryForm__main__auth-container__auth EntryForm__main__auth-container__auth--demo"
+            onClick={() => {
+              setDemoClicked(true);
+              clickDemo();
+            }}
+          >
+            <div>
+              <DemoSvg style={{ width: "25px" }} />
+            </div>
+            <div>Use Demo Account</div>
+            <div></div>
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Welcome;
